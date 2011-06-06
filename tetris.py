e3d5f9a03ea2679555ad3f5fd54ffd28d6e993e5
tommy <tommy@chestnut.(none)>
2011-06-05 20:35:50 -0400

    basic OO code

--- tetris.py
+++ tetris.py
@@ -1,89 +1,80 @@
 #!/usr/bin/env python
 
+from random import randint
 from Tkinter import *
 
-def create_pieces():
-    tetros = []
-    sPiece = [
-        [ 0, 1, 1 ],
-        [ 1, 1, 0 ]
-        ]
-    zPiece = [
-        [ 2, 2, 0 ],
-        [ 0, 2, 2 ]
-        ]
-    lPiece = [
-        [ 0, 0, 3 ],
-        [ 3, 3, 3 ]
-        ]
-    jPiece = [
-        [ 4, 0, 0 ],
-        [ 4, 4, 4 ]
-        ]
-    tPiece = [
-        [ 0, 5, 0 ],
-        [ 5, 5, 5 ]
-        ]
-    iPiece = [
-        [ 6, 6, 6, 6 ]
-        ]
-    oPiece = [
-        [ 7, 7 ],
-        [ 7, 7 ]
-        ]
-    tetros.append(sPiece)
-    tetros.append(zPiece)
-    tetros.append(lPiece)
-    tetros.append(jPiece)
-    tetros.append(tPiece)
-    tetros.append(iPiece)
-    tetros.append(oPiece)
-    
-    return tetros
+class Tetris(object):
+    def loadBoard(self, rows, cols):
+        new_board = []
+        for i in range(rows):
+            row = []
+            for j in range(cols):
+                row.append(0)
+            new_board.append(row)
+        print new_board
+        return new_board
 
-def loadBoard(rows, cols):
-    new_board = []
-    for i in range(rows):
-        row = []
-        for j in range(cols):
-            row.append(0)
-        new_board.append(row)
-    return new_board
+    def __init__(self,root):
+        rows=15
+        cols=10
+        self.board = self.loadBoard(rows, cols)
+        self.canvas = Canvas(root, width=310, height=465)
+        self.canvas.pack()
+        self.pieces = [
+            [[ 0, 1, 1 ], [ 1, 1, 0 ]],
+            [[ 2, 2, 0 ], [ 0, 2, 2 ]],
+            [[ 0, 0, 3 ], [ 3, 3, 3 ]],
+            [[ 4, 0, 0 ], [ 4, 4, 4 ]],
+            [[ 0, 5, 0 ], [ 5, 5, 5 ]],
+            [[ 6, 6, 6, 6 ]],
+            [[ 7, 7 ], [ 7, 7 ]]   
+            ]
+        self.x_pos = 4
+        self.y_pos = 0
+        self.current_piece = 0
 
 def drawCell(left, top, right, bottom, value):
     if (value == 0):
         fill_color = "blue"
     else:
         fill_color = "yellow"
-        
-    canvas.create_rectangle(left, top, right, bottom, fill=fill_color)
+    tetris.canvas.create_rectangle(left, top, right, bottom, fill=fill_color)
 
-def drawBoard(board):
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            value = board[i][j]
+def drawBoard():
+    for i in range(len(tetris.board)):
+        for j in range(len(tetris.board[i])):
+            value = tetris.board[i][j]
             left = i * 31
             top = j * 31
             right = i * 31 + 31
             bottom = j * 31 + 31            
             drawCell(left, top, right, bottom, value)
+    print tetris.board
 
-def drawPiece(board, piece, x, y):
+def drawPiece():
+    piece = tetris.pieces[tetris.current_piece]
     for i in range(len(piece)):
-        for j in range(len(piece)):
-            this_x = x + i
-            this_y = y + j
-            board[this_x][this_y] = piece[i][j]
+        for j in range(len(piece[i])):
+            this_x = tetris.x_pos + i
+            this_y = tetris.y_pos + j
+            tetris.board[this_x][this_y] = piece[i][j]
+    drawBoard()
 
-    drawBoard(board)
-
-def movePiece(board, piece, old_x, old_y, x, y):
+def movePiece(old_x, old_y):
+    piece = tetris.pieces[tetris.current_piece]
     for i in range(len(piece)):
-        for j in range(len(piece)):
+        for j in range(len(piece[i])):
             this_x = old_x + i
             this_y = old_y + j
-            board[this_x][this_y] = 0
-    drawPiece(board, piece, x, y)
+            tetris.board[this_x][this_y] = 0
+    drawPiece()
+
+def newPiece():
+    piece_num = randint(0,(len(tetris.pieces)-1))
+    tetris.current_piece = piece_num
+    tetris.x_pos = 4
+    tetris.y_pos = 0
+    drawPiece()
 
 def keyPressed(event):
     if (event.keysym =="Up"):
@@ -93,33 +84,26 @@ def keyPressed(event):
         # moveFallingPiece(+1, 0)
         pass
     elif (event.keysym=="Left"):
-        old_x = x
-        old_y = y
-        x += 1
-        y += 1
-        movePiece(board, piece, old_x, old_y, x, y)
+        old_x = tetris.x_pos
+        old_y = tetris.y_pos
+        tetris.x_pos -= 1
+        movePiece(old_x, old_y)
     elif (event.keysym =="Right"):
-        old_x = x
-        old_y = y
-        x -= 1
-        y -= 1
-        movePiece(board, piece, old_x, old_y, x, y)
+        old_x = tetris.x_pos
+        old_y = tetris.y_pos
+        tetris.x_pos += 1
+        movePiece(old_x, old_y)
     elif (event.keysym =="r"):
         pass
         # restartGame()
-                  
+
 def run():
     root = Tk()
-    global canvas
-    canvas = Canvas(root, width=310, height=465)
-    canvas.pack()
-    board = loadBoard(10,15)
-    tetros = create_pieces()
-    piece = tetros[6]
-    x = 4
-    y = 0
+    global tetris
+    tetris = Tetris(root)
+    newPiece()
     root.bind("<Key>", keyPressed)
     root.mainloop()
 
 run()
-#drawPiece(board,0,4)
+
